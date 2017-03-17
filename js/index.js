97613c9479fe748b42db2b246ae67e428746c232
Rafael Casabianca <rcasabianca1@sheffield.ac.uk>
2017-03-17 17:52:19 +0000

    First Commit
    Mysql connection works

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection(
+    {
+      host     : 'stusql.dcs.shef.ac.uk',
+      port     : '3306',
+      user     : 'team064',
+      password : 'ec4c7a0d',
+      database : 'team064'
+    }
+);
+
+connection.connect();
+var query = connection.query('SELECT name FROM football');
+
+query.on('error', function(err) {
+    throw err;
+});
+
+query.on('fields', function(fields) {
+    // console.log(fields);
+});
+
+query.on('result', function(row) {
+    console.log(row);
+});
+connection.end();
